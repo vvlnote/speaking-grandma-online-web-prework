0078e8dd18384222d191a3816a35ffd8e154f7ba
Joy Lin <vvlnote@hotmail.com>
2019-02-14 06:03:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -25,5 +25,7 @@ def speack_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase.upcase == phrase)
+    return "NO, NOT SINCE 1938!"
+  else
     return ""
 end
